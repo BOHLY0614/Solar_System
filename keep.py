ba702f66cccef874754cb5e8306957835f770a36
BOHLY <nicolas.bohly@estaca.eu>
2022-10-08 20:18:18 +0200

    init

---
+++ keep.py
@@ -0,0 +1,12 @@
+    
+    #T[0].set_data(exdata,eydata)
+    #T[1].set_data(exlist[k],eylist[k])
+    #T[2].set_position((exlist[k],eylist[k]))
+    #cas de Mars
+    #Ma[0].set_data(maxdata,maydata)
+    #Ma[1].set_data(maxlist[k],maylist[k])
+    #Ma[2].set_position((maxlist[k],maylist[k]))
+    #cas de jupiter
+    #J[0].set_data(jxdata,jydata)
+    #J[1].set_data(jxlist[k],jylist[k])
+    #J[2].set_position((jxlist[k],jylist[k]))
